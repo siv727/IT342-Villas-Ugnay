492e1f85a1229548345646d6a13624ac94657e3c
siv-a-siv <verinkai727@gmail.com>
2026-03-07 22:35:05 +0800

    feat(service): add CustomUserDetailsService

---
+++ ugnay/src/main/java/edu/cit/villas/ugnay/service/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package edu.cit.villas.ugnay.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import edu.cit.villas.ugnay.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
