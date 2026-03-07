b06d23f24aefd2d850c8b026e4d63d8483893fbe
siv-a-siv <verinkai727@gmail.com>
2026-03-07 22:23:25 +0800

    feat(dto): add registration, login, and response DTOs

---
+++ ugnay/src/main/java/edu/cit/villas/ugnay/dto/LoginResponse.java
@@ -0,0 +1,12 @@
+package edu.cit.villas.ugnay.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class LoginResponse {
+    private Long userId;
+    private String role;
+    private String message;
+}
