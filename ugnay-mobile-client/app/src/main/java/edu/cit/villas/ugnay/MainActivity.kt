aba6f8b55425d6158f61efd62b2de7e045671e40
siv-a-siv <verinkai727@gmail.com>
2026-03-29 01:11:37 +0800

    feat(mobile-home): add role-based home, tabs, search, and logout flow

---
+++ ugnay-mobile-client/app/src/main/java/edu/cit/villas/ugnay/MainActivity.kt
@@ -0,0 +1,14 @@
+package edu.cit.villas.ugnay
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import edu.cit.villas.ugnay.activities.LoginActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, LoginActivity::class.java))
+        finish()
+    }
+}
