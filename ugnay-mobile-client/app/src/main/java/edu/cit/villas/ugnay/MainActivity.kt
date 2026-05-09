c7e8b64c1f5bd923b4f7da4e7e9ac67d2eed9e4c
siv-a-siv <verinkai727@gmail.com>
2026-05-09 21:16:46 +0800

    Project Vertical Slice Refactoring Complete

--- ugnay-mobile-client/app/src/main/java/edu/cit/villas/ugnay/MainActivity.kt
+++ ugnay-mobile-client/app/src/main/java/edu/cit/villas/ugnay/MainActivity.kt
@@ -3,7 +3,7 @@ package edu.cit.villas.ugnay
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import edu.cit.villas.ugnay.activities.LoginActivity
+import edu.cit.villas.ugnay.features.auth.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
