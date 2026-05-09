8427483a330b4b09fa05dedcd4984c1d352bff1d
siv-a-siv <verinkai727@gmail.com>
2026-05-09 18:35:04 +0800

    fix(sample-request): replace @Data with @Getter/@Setter and remove duplicate brace in Product

--- ugnay/src/main/java/edu/cit/villas/ugnay/entity/Product.java
+++ ugnay/src/main/java/edu/cit/villas/ugnay/entity/Product.java
@@ -14,10 +14,12 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @Entity
 @Table(name = "products")
@@ -57,5 +59,4 @@ public class Product {
     @CollectionTable(name = "product_images", joinColumns = @JoinColumn(name = "product_id"))
     @Column(name = "image_url")
     private List<String> imageUrls;
-}
 }
