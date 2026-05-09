eb7f5cc87240142c1a7e5da4696ab42cdedd1667
siv-a-siv <verinkai727@gmail.com>
2026-05-09 18:35:13 +0800

    fix(sample-request): replace @Data with @Getter/@Setter in Connection to prevent jpa recursion

--- ugnay/src/main/java/edu/cit/villas/ugnay/entity/Connection.java
+++ ugnay/src/main/java/edu/cit/villas/ugnay/entity/Connection.java
@@ -13,10 +13,12 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @Entity
 @Table(name = "connections", uniqueConstraints = {
