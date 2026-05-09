5c452d6af82a0298ba1a3ec35920df6c18291532
siv-a-siv <verinkai727@gmail.com>
2026-05-09 18:35:39 +0800

    fix(sample-request): replace @Data with @Getter/@Setter and remove duplicate brace in SampleRequestItem

--- ugnay/src/main/java/edu/cit/villas/ugnay/entity/SampleRequestItem.java
+++ ugnay/src/main/java/edu/cit/villas/ugnay/entity/SampleRequestItem.java
@@ -9,10 +9,12 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @Entity
 @Table(name = "sample_request_items")
@@ -33,5 +35,4 @@ public class SampleRequestItem {
 
     @Column(nullable = false)
     private Integer quantity;
-}
 }
